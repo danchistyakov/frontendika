7b46254ccc32a77aa8815ede572b8f96c4bc7ce9
Daniil Chistyakov <4i.danila@gmail.com>
2021-05-18 09:15:03 +0300

    Bug fixed

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer>
-            <p>© 2021, Built with ❤ & <a href="https://buttercms.com/" target='_blank' noopener noreferrer><img className='butter_logo' src='https://cdn.buttercms.com/PGJPyIwaQ2KnOA8UyKfH'></img></a> by Daniil Chistyakov.</p>
+            <p>© 2021, Built with ❤ & <a href="https://buttercms.com/" target='_blank' rel='noopener noreferrer'><img className='butter_logo' src='https://cdn.buttercms.com/PGJPyIwaQ2KnOA8UyKfH'></img></a> by Daniil Chistyakov.</p>
         </footer>
     )
 }
